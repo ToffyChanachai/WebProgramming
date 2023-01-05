155a987d3aa8dbc7241e7425e5c004d3d4f484a6
ToffyChanachai <toffy.chanachai123@gmail.com>
2023-01-05 09:18:25 +0700

    update

---
+++ ch 4/variable.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>การกำหนดค่าตัวแปร</title></head>
+<body>
+<?php
+    $x = 100;
+    $y = "50";
+    $z = '300';
+    $add = $x + $y;
+    $sub = $z - $y;
+    $message = "Hello World";
+    echo $x; echo "<br />";
+    echo $y; echo "<br />";
+    echo $z; echo "<br />";
+    echo $add; echo "<br />";
+    echo $sub; echo "<br />";
+    echo $message; echo "<br />";
+?>
+</body>
+</html>
